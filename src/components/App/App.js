8f5907195f340ec732eb8fa1bb512ae844bfa567
GeometricEath <geo.fsv@gmail.com>
2023-09-06 17:57:44 +0300

    feat: Подключена маршрутизация

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Main from '../Main/Main';
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="page">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
