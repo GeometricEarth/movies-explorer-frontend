982f49d7b36add5e434fdcb731d1dcb48d8d8e0a
Geometr <geo.fsv.p@gmail.com>
2023-09-10 23:42:22 +0300

    inwork: Добавлен блок techs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import './Main.css';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 export default function Main() {
   return (
     <>
       <Promo></Promo>
       <AboutProject></AboutProject>
+      <Techs></Techs>
     </>
   );
 }
---
+++ src/components/Techs/Techs.jsx
@@ -0,0 +1,24 @@
+import './Techs.css';
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+export default function Techs() {
+  return (
+    <section className='tech'>
+      <SectionTitle>Технологии</SectionTitle>
+      <h3 className="tech__subtitle">7 технологий</h3>
+      <p className='tech__text'>
+        На курсе веб-разработки мы освоили технологии, которые применили в
+        дипломном проекте.
+      </p>
+      <ul className='tech__list'>
+        <li className='tech__list-item'>HTML</li>
+        <li className='tech__list-item'>CSS</li>
+        <li className='tech__list-item'>JS</li>
+        <li className='tech__list-item'>React</li>
+        <li className='tech__list-item'>Git</li>
+        <li className='tech__list-item'>Express.js</li>
+        <li className='tech__list-item'>mongoDB</li>
+      </ul>
+    </section>
+  );
+}
