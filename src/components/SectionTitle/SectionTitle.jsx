4be2cd474c7c9e007753bce76b1c243aa827056e
GeometricEath <geo.fsv@gmail.com>
2023-09-11 15:06:15 +0300

    feat: Подчеркивание заголовка секции перенесено в отдльеный класс

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 import './SectionTitle.css';
 
-export default function SectionTitle({children}) {
+export default function SectionTitle({ classList = 'section-title__underline_color_gray', children }) {
   return (
-    <h2 className="section-title">{children}</h2>
+    <h2 className={"section-title " + classList}>{children}</h2>
   )
 }
