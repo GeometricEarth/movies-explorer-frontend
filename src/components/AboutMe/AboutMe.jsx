6e7d85b28d49bfc337aa241b848118987960e31f
GeometricEath <geo.fsv@gmail.com>
2023-09-12 17:43:00 +0300

    feat: Релизовать блок about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,14 +7,13 @@ export default function AboutMe() {
   return (<section className='about-me'>
     <SectionTitle>Студент</SectionTitle>
     <div className='about-me__wraper'>
-      <div>
+      <div className='about-me__column'>
         <h2 className='about-me__name'>Сергей</h2>
         <p className='about-me__ocupation'>Фронтенд-разработчик, 31 год</p>
-        <p className='about-me__about'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-          и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+        <p className='about-me__about'>Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ. У&nbsp;меня есть жена
+          и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить. С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;. После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.</p><Link to='https://github.com/GeometricEarth' className='about-me__github-link'>Github</Link>
       </div>
       <img className='about-me_image' src={StudentFoto} alt="Фотография студента" />
     </div>
-    <Link to='https://github.com/GeometricEarth' className='about-me__github-link'>Github</Link>
   </section>);
 }
