c36fc3bdea9711ed0b4564e8db4765b790ce4269
GeometricEath <geo.fsv@gmail.com>
2023-09-21 17:37:37 +0300

    fix: Исправлено смещение  центрального блока навигации

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import './Header.css';
-import logo from '../../images/logo.svg';
+// import logo from '../../images/logo.svg';
 import Navigation from '../Navigation/Navigation';
 import { Link } from 'react-router-dom';
 
@@ -7,8 +7,8 @@ export default function Header() {
   const isAuthorized = true;
   return (
     <header className="header">
-      <Link to="/">
-        <img className="header__logo" src={logo} alt="Логотип проекта" />
+      <Link className="header__logo" to="/">
+        {/* <img className="header__logo" src={logo} alt="Логотип проекта" /> */}
       </Link>
       <Navigation isAuthorized={isAuthorized}></Navigation>
     </header>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,29 +1,52 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './Navigation.css';
 import Button from '../Button/Button';
-import burgerButton from '../../images/burgerButton.svg'
 
 export default function Navigation({ isAuthorized }) {
   const navigate = useNavigate();
-  const handleClickProfile = ()=>{
+  const handleClickProfile = () => {
     navigate('/profile');
-  }
-  const handleClickSignIn= ()=>{
+  };
+  const handleClickSignIn = () => {
     navigate('/signin');
-  }
-  return (<>
-    {isAuthorized && (<><nav className='navigation'>
-      <Link className="navigation__link" to="/movies">Фильмы</Link>
-      <Link className="navigation__link" to="/saved-movies">Сохранённые фильмы</Link>
-    </nav>
-      <Button classList='navigation__profile-button' onClick={handleClickProfile}>Аккаунт</Button>
+  };
+  return (
+    <>
+      {isAuthorized && (
+        <>
+          <nav className="navigation">
+            <Link className="navigation__link" to="/movies">
+              Фильмы
+            </Link>
+            <Link className="navigation__link" to="/saved-movies">
+              Сохранённые фильмы
+            </Link>
+          </nav>
+          <Button
+            classList="auth-buttons auth-buttons_type_profile"
+            onClick={handleClickProfile}
+          >
+            Аккаунт
+          </Button>
+        </>
+      )}
+      {!isAuthorized && (
+        <nav className='auth-buttons'>
+          <Link className="auth-buttons_type_sign-up" to="/signup">
+            Регистрация
+          </Link>
+          <Button
+            classList="auth-buttons_type_sign-in"
+            onClick={handleClickSignIn}
+          >
+            Войти
+          </Button>
+        </nav>
+      )}
+      {isAuthorized && (
+        <Button classList="navigation__burger-menu-button">
+        </Button>
+      )}
     </>
-    )}
-    {!isAuthorized && (<nav>
-      <Link className='navigation__sign-up' to="/signup">Регистрация</Link>
-      <Button classList="navigation__sign-in-button" onClick={handleClickSignIn}>Войти</Button>
-    </nav>)}
-    {isAuthorized &&  (<Button classList="navigation__burger-menu-button"><img src={burgerButton} alt='Открыть меню навигации' /></Button>)}
-  </>
-  )
+  );
 }
