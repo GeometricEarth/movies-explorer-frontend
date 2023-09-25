a6bce290398b3d73838a4ff35e4674ec4550ed0e
GeometricEath <geo.fsv@gmail.com>
2023-09-25 09:58:18 +0300

    ref: Добавлены header и footer внутрь компонента

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,16 +1,19 @@
 import './Movies.css';
+import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
 
 import movies from '../../utils/mockData';
 
 export default function Movies() {
-  // const [isLoaded, setIsLoaded] = useState(true);
   return (<>
+    <Header></Header>
     <div className='movies'>
       <SearchForm></SearchForm>
       <MoviesCardList moviesList={movies}></MoviesCardList>
     </div>
+    <Footer></Footer>
   </>
   )
 }
