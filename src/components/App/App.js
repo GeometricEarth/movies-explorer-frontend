66118d4defa04199213ecb14df10353b499a8672
GeometricEath <geo.fsv@gmail.com>
2023-09-25 08:58:25 +0300

    ref: Перенесен header и footer внутрь Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,13 @@
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from '../Main/Main';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-
-import './App.css';
 
 function App() {
   return (
     <div className="page">
-      <Header></Header>
       <Routes>
         <Route path="/" element={<Main />}></Route>
       </Routes>
-      <Footer></Footer>
     </div>
   );
 }
