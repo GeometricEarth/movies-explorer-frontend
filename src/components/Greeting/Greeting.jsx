06575db9d4b81035ed38f392e744e26c1c2f32ca
GeometricEath <geo.fsv@gmail.com>
2023-09-25 17:18:37 +0300

    feat: Реализовать страницу входа signin

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -3,10 +3,10 @@ import logo from '../../images/logo.svg';
 import { Link } from 'react-router-dom';
 
 
-export default function Greeting({children}) {
+export default function Greeting({ children }) {
   return (
     <div className='greeting'>
-      <Link to="/">
+      <Link className='greeting__link' to="/">
         <img className="logo" src={logo} alt="Логотип проекта" />
       </Link>
       <p className='greeting__text'>{children}</p>
