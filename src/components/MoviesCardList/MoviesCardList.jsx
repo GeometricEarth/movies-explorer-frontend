4026f9d145460feaa3ffe478c1fd14a3dae722f2
Geometr <geo.fsv.p@gmail.com>
2023-10-22 21:43:44 +0300

    feat: Реализовать страницу сохраненных фильмов

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,23 +1,26 @@
-import './MoviesCardList.css'
+import './MoviesCardList.css';
 
 import Preloader from '../Preloader/Preloader';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
-function MoviesCardList({ moviesList }) {
+function MoviesCardList({ moviesList, isSavedMovies, onSave, onDelete }) {
   return (
     <section className="movies-list">
       {!moviesList && <Preloader></Preloader>}
-      <div className='movies-list__card-wrapper'>
-        {moviesList.map((moviesCard) => {
-          return (
-            <MoviesCard
-              movieCard={(moviesCard)}
-              key={moviesCard.movieId}
-            ></MoviesCard>
-          );
-        })}
+      <div className="movies-list__card-wrapper">
+        {moviesList &&
+          moviesList.map((movieCard) => {
+            return (
+              <MoviesCard
+                movieCard={movieCard}
+                isSavedMovies={isSavedMovies}
+                onSave={onSave}
+                onDelete={onDelete}
+                key={movieCard.movieId}
+              ></MoviesCard>
+            );
+          })}
       </div>
-      <button className='movies-list__more-button' type="button">Ещё</button>
     </section>
   );
 }
