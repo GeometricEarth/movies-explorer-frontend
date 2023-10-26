286cf6442012b95a4f6f619d4c03d5d6690af528
Geometr <geo.fsv.p@gmail.com>
2023-10-26 20:35:30 +0300

    style

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -3,10 +3,16 @@ import Button from '../Button/Button';
 
 export default function NavTab() {
   return (
-    <nav className='navtab'>
-      <a href="#about"><Button classList='navtab__button'>О проекте</Button></a>
-      <a href="#tech"><Button classList='navtab__button'>Технологии</Button></a>
-      <a href="#student"><Button classList='navtab__button'>Студент</Button></a>
+    <nav className="navtab">
+      <a href="#about">
+        <Button classList="navtab__button">О проекте</Button>
+      </a>
+      <a href="#tech">
+        <Button classList="navtab__button">Технологии</Button>
+      </a>
+      <a href="#student">
+        <Button classList="navtab__button">Студент</Button>
+      </a>
     </nav>
   );
 }
