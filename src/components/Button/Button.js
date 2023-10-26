3c00017f5318273e92931254b0fbd810467cfc19
Geometr <geo.fsv.p@gmail.com>
2023-10-26 20:26:16 +0300

    style: исправлено форматирование

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,19 @@
 import './Button.css';
 
-export default function Button({ classList, type = 'button', onClick, children, disabled }) {
-
+export default function Button({
+  classList,
+  type = 'button',
+  onClick,
+  children,
+  disabled,
+}) {
   return (
-    <button className={'button ' + classList} type={type} onClick={onClick} disabled={disabled}>
+    <button
+      className={'button ' + classList}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -3,21 +3,23 @@ import SectionTitle from '../SectionTitle/SectionTitle';
 
 export default function Techs() {
   return (
-    <section className='tech'>
-      <SectionTitle classList='section-title__underline_color_black'>Технологии</SectionTitle>
+    <section className="tech">
+      <SectionTitle classList="section-title__underline_color_black">
+        Технологии
+      </SectionTitle>
       <h3 className="tech__subtitle">7 технологий</h3>
-      <p className='tech__text'>
+      <p className="tech__text">
         На курсе веб-разработки мы освоили технологии, которые применили в
         дипломном проекте.
       </p>
-      <ul className='tech__list'>
-        <li className='tech__list-item'>HTML</li>
-        <li className='tech__list-item'>CSS</li>
-        <li className='tech__list-item'>JS</li>
-        <li className='tech__list-item'>React</li>
-        <li className='tech__list-item'>Git</li>
-        <li className='tech__list-item'>Express.js</li>
-        <li className='tech__list-item'>mongoDB</li>
+      <ul className="tech__list">
+        <li className="tech__list-item">HTML</li>
+        <li className="tech__list-item">CSS</li>
+        <li className="tech__list-item">JS</li>
+        <li className="tech__list-item">React</li>
+        <li className="tech__list-item">Git</li>
+        <li className="tech__list-item">Express.js</li>
+        <li className="tech__list-item">mongoDB</li>
       </ul>
     </section>
   );
