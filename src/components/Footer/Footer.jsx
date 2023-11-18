c3f759592a734c55181a771be16ab93982b098b6
Sergey <geo.fsv@gmail.com>
2023-11-18 22:35:25 +0300

    feat: Добавлены ссылки в футере

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,23 @@
-import './Footer.css'
+import './Footer.css';
+import LinkStyled from '../LinkStyled/LinkStyled'
 
 export default function Footer() {
   return (
-    <footer className='footer'>
-      <h2 className='footer__project-name'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-      <div className='footer__wraper'>
-        <p className='footer__copyright'>© 2023</p>
-        <ul className='footer__links-list'>
-          <li className='footer__link'>Яндекс.Практикум</li>
-          <li className='footer__link'>Github</li>
+    <footer className="footer">
+      <h2 className="footer__project-name">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h2>
+      <div className="footer__wraper">
+        <p className="footer__copyright">© 2023</p>
+        <ul className="footer__links-list">
+          <li>
+            <LinkStyled className="footer__link" to="https://practicum.yandex.ru/">Яндекс.Практикум</LinkStyled>
+          </li>
+          <li>
+            <LinkStyled className="footer__link" to="https://github.com/GeometricEarth">Github</LinkStyled>
+          </li>
         </ul>
       </div>
     </footer>
-  )
+  );
 }
