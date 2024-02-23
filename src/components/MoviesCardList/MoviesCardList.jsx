79533c737935fd7d771cd45ae0d351df8371641e
GeometricEath <geo.fsv@gmail.com>
2024-02-23 23:15:00 +0300

    feat: Релизовать добавление рядов карточек

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,15 +1,55 @@
 import './MoviesCardList.css';
+import { useEffect, useState } from 'react';
 
 import Preloader from '../Preloader/Preloader';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 function MoviesCardList({ moviesList, isSavedMovies, onSave, onDelete }) {
+  const [displayedCards, setDisplayedCards] = useState([]);
+  const [rowsCount, setRowsCount] = useState(4);
+
+  useEffect(() => {
+    const setupCardGrid = () => {
+      const viewWidth = document.documentElement.clientWidth;
+      let cardInRow = 3;
+
+      console.log(viewWidth);
+
+      if (viewWidth < 1280) {
+        cardInRow = 2;
+      }
+      if (viewWidth < 690) {
+        if (rowsCount < 5) {
+          setRowsCount(5);
+        }
+        cardInRow = 1;
+      }
+
+      setDisplayedCards(moviesList.slice(0, cardInRow * rowsCount));
+    };
+
+    const handleResize = () => {
+      setupCardGrid();
+    };
+
+    window.addEventListener('resize', handleResize);
+    setupCardGrid();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [moviesList, rowsCount]);
+
+  const handleMoreClick = () => {
+    setRowsCount(rowsCount + 1);
+  };
+
   return (
     <div className="movies-list">
-      {!moviesList && <Preloader></Preloader>}
+      {!displayedCards && <Preloader></Preloader>}
       <div className="movies-list__card-wrapper">
-        {moviesList &&
-          moviesList.map((movieCard) => {
+        {displayedCards &&
+          displayedCards.map((movieCard) => {
             return (
               <MoviesCard
                 movieCard={movieCard}
@@ -21,6 +61,13 @@ function MoviesCardList({ moviesList, isSavedMovies, onSave, onDelete }) {
             );
           })}
       </div>
+      <button
+        className="button movies-list__more-button"
+        type="button"
+        onClick={handleMoreClick}
+      >
+        Ещё
+      </button>
     </div>
   );
 }
