0c9ec6010750a16ea0446f285af61ef348941e6a
GeometricEath <geo.fsv@gmail.com>
2024-03-18 08:36:10 +0300

    feat: Базовый функционал страницы сохраненных фильмов и обработка ошибок

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -3,36 +3,88 @@ import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 import { useEffect, useState } from 'react';
-import { getSavedMovies } from '../../utils/MainApi';
+import { getSavedMovies, deleteMovie } from '../../utils/MainApi';
 
 export default function SavedMovies() {
   const [savedMovies, setSavedMovies] = useState([]);
-  const [isShorts, setShorts] = useState(true);
+  const [filteredList, setFilteredList] = useState([]);
+  const [isShorts, setShorts] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
+    setShorts(!!Number(localStorage.getItem('isShortsSaved')));
     getSavedMovies()
       .then((movies) => {
-
         setSavedMovies(movies);
+        setFilteredList(movies);
       })
       .catch(console.log);
   }, []);
 
-  useEffect(() => {
-    if (isShorts) {
-      savedMovies.filter((movie) => movie.duration < 40);
-    }
-  }, [isShorts]);
+  const handleDelete = (id) => {
+    deleteMovie(id)
+      .then(() => {
+        setSavedMovies((state) => {
+          return state.filter((movie) => {
+            return movie.movieId !== id;
+          });
+        });
+        setFilteredList((state) => {
+          return state.filter((movie) => {
+            return movie.movieId !== id;
+          });
+        });
+      })
+      .catch(async (resp) => {
+        try {
+          if (resp instanceof TypeError) {
+            const errMessage =
+              'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
+            console.error(errMessage);
+            setError(errMessage);
+            return;
+          }
+          const jsonBody = await resp.json();
+          setError(jsonBody.message);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+  };
 
-  const handleDelete = (cardId) => {
-    console.log(cardId);
+  const filtrateShorts = (list) => {
+    return list.filter((movie) => movie.duration < 40);
+  };
+
+  const filtrateByName = (list, query) => {
+    const loweredQuery = query.toLowerCase();
+    return list.filter((movie) => {
+      return (
+        movie.nameRU.toLowerCase().includes(loweredQuery) ||
+        movie.nameEN.toLowerCase().includes(loweredQuery)
+      );
+    });
+  };
+
+  const handleSetShorts = (value) => {
+    localStorage.setItem('isShortsSaved', value ? 1 : 0);
+    setShorts(value);
+  };
+
+  const handleSearch = (query) => {
+    setFilteredList(filtrateByName(savedMovies, query));
   };
 
   return (
     <main className="movies">
-      <SearchForm isShorts={isShorts} setShorts={setShorts}></SearchForm>
+      <SearchForm
+        isShorts={isShorts}
+        setShorts={handleSetShorts}
+        onSubmit={handleSearch}
+      ></SearchForm>
+      <div className="movies__message">{error}</div>
       <MoviesCardList
-        moviesList={savedMovies}
+        moviesList={isShorts ? filtrateShorts(filteredList) : filteredList}
         isSavedMovies={true}
         onDelete={handleDelete}
       ></MoviesCardList>
